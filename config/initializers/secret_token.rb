0de7ccf8108217374e47dde1f42a049e667ec72b
Andrew Kvalheim <Andrew@Kvalhe.im>
2014-02-19 18:20:48 -0800

    Initialize Rails 4.0.3 app with PostgreSQL.
        rails new howto.email --database=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HowtoEmail::Application.config.secret_key_base = '705c3369b11a61a758f921b230676f3bb5391aa6d64f6da9ef9708ad9dd654c4c2360d392c2c794d70e9d614454ac6d09fe87135625f0634228787bb36aadcfc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HowtoEmail::Application.config.session_store :cookie_store, key: '_howto_email_session'
