aff74fd8f0b4e39fdd1c3ccdd4e727be1bafe133
Andrew Kvalheim <Andrew@Kvalhe.im>
2014-02-24 15:02:31 -0800

    Add ability to check web of trust.

--- app/helpers/identities_helper.rb
+++ app/helpers/identities_helper.rb
@@ -1,2 +1,15 @@
+require 'shellwords'
+
 module IdentitiesHelper
+  REFERENCE = 'B80C4E1E6F5544B277518173535B253E3B5AB9C6'
+  WOTSAP = Shellwords.escape(Rails.root.join('wotsap').to_s)
+
+  def connected_to?(fingerprint)
+    Rails.cache.fetch("/wot/#{ fingerprint }", expires_in: 1.day) do
+      from = Shellwords.escape(REFERENCE[-8..-1])
+      to   = Shellwords.escape(fingerprint[-8..-1])
+
+      system("#{ WOTSAP } #{ from } #{ to }", err: File::NULL, out: File::NULL)
+    end
+  end
 end
--- spec/helpers/identities_helper_spec.rb
+++ spec/helpers/identities_helper_spec.rb
@@ -11,4 +11,26 @@ require 'spec_helper'
 #   end
 # end
 describe IdentitiesHelper do
+  describe 'web of trust detection' do
+    let(:collision)   { 'D00436A90C4BD12002020A3C37E1C17570096AD1' }
+    let(:connected)   { 'BE2B972FE80E101AAF828BB5F4D89A1810B196EF' }
+    let(:nonexistent) { 'A4E2B0C325633D2120BEC0C6CD9E545436171BC6' }
+    let(:reference)   { 'B80C4E1E6F5544B277518173535B253E3B5AB9C6' }
+
+    it 'detects connection to a connected key' do
+      expect(connected_to?(connected)).to be_true
+    end
+
+    it 'detects connection to a key that has a collision' do
+      expect(connected_to?(collision)).to be_true
+    end
+
+    it 'detects connection to itself' do
+      expect(connected_to?(reference)).to be_true
+    end
+
+    it 'does not detect connection to a nonexistent key' do
+      expect(connected_to?(nonexistent)).to be_false
+    end
+  end
 end
