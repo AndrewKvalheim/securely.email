0d0a1271d320d3a154f0c0bff1cd56efa1154aed
Andrew Kvalheim <Andrew@Kvalhe.im>
2014-09-18 14:16:48 -0700

    Move secret_key_base out of version control.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SecurelyEmail::Application.config.secret_key_base = '705c3369b11a61a758f921b230676f3bb5391aa6d64f6da9ef9708ad9dd654c4c2360d392c2c794d70e9d614454ac6d09fe87135625f0634228787bb36aadcfc'
+SecurelyEmail::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
